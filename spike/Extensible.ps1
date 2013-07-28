e39a87ce9eeb4224c1bbf69f80c7610dfef16a83
Doug Finke <finked@hotmail.com>
2013-07-28 08:42:58 -0400

    New animated gif

---
+++ spike/Extensible.ps1
@@ -0,0 +1,42 @@
+﻿function Select-Ast {
+    param(
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $FullName,
+        #[ValidateSet('AssignmentStatement','FunctionDefinition')]
+        $ASTType='AssignmentStatement'
+    )
+
+    Process {
+        
+        $TargetType = "System.Management.Automation.Language.$($ASTType)Ast" -as [Type]
+        $ast=[System.Management.Automation.Language.Parser]::ParseFile( (Resolve-Path $FullName) , [ref]$null, [ref]$null)
+        
+        foreach($item in $ast.FindAll({ param($p) $p -is $TargetType }, $true)) {
+             & ("Process{0}" -f $item.gettype().name) $item
+        }
+    }
+}
+
+function ProcessAssignmentStatementAst ($AssignmentStatementAst){
+    [PSCustomObject]@{
+        Name=$item.Left
+        File=$item.Extent.File
+        Text=$item.Extent.Text
+    }
+}
+
+function ProcessFunctionDefinitionAst ($item) {
+    [PSCustomObject]@{
+        Name=$item.Name
+        Text=$item.Extent.Text
+        File=$item.Extent.File
+    }
+}
+
+function ProcessCommandAst ($item) {
+    $item
+}
+
+cls
+
+dir *.ps1 | Select-Ast -ASTType FunctionDefinition # Command
