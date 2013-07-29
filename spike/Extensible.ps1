dc34099c572f38bc151a4fc5714b0bf0d00a9b7e
Doug Finke <finked@hotmail.com>
2013-07-28 21:08:08 -0400

    Initial Commit

--- spike/Extensible.ps1
+++ spike/Extensible.ps1
@@ -3,7 +3,8 @@
         [Parameter(ValueFromPipelineByPropertyName)]
         $FullName,
         #[ValidateSet('AssignmentStatement','FunctionDefinition')]
-        $ASTType='AssignmentStatement'
+        #$ASTType='AssignmentStatement'
+        $ASTType
     )
 
     Process {
@@ -17,19 +18,24 @@
     }
 }
 
-function ProcessAssignmentStatementAst ($AssignmentStatementAst){
+function Get-ExtentInfo ($targetExtent) {
     [PSCustomObject]@{
-        Name=$item.Left
-        File=$item.Extent.File
-        Text=$item.Extent.Text
+        Line              = $targetExtent.Extent.Text
+        StartLineNumber   = $targetExtent.Extent. StartLineNumber  
+        StartColumnNumber = $targetExtent.Extent.StartColumnNumber
+        EndLineNumber     = $targetExtent.Extent.EndLineNumber
+        EndColumnNumber   = $targetExtent.Extent.EndColumnNumber
+        FileName          = Split-Path -Leaf $targetExtent.Extent.File
+        FullName          = $targetExtent.Extent.File
     }
 }
 
 function ProcessFunctionDefinitionAst ($item) {
     [PSCustomObject]@{
-        Name=$item.Name
-        Text=$item.Extent.Text
-        File=$item.Extent.File
+        Name     = $item.Name
+        Text     = $item.Extent.Text
+        FullName = $item.Extent.File
+        Type     = "Function"
     }
 }
 
@@ -37,6 +43,83 @@ function ProcessCommandAst ($item) {
     $item
 }
 
+function Find-Function {
+    param(
+        $Name,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $FullName
+    )
+
+    Process {
+        Select-Ast $FullName -ASTType FunctionDefinition | Where { $_.Name -match $Name}
+    }
+}
+
+function Find-FunctionUsage {
+    param(
+        $Name,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $Fullname
+    )
+
+    Process {
+        Select-Ast -FullName $FullName -ASTType Command | 
+            Where {$_.CommandElements.Extent.Text -match $Name} |
+            ForEach {
+                [PSCustomObject]@{
+                    Line              = $_.Extent.Text
+                    StartLineNumber   = $_.Extent. StartLineNumber  
+                    StartColumnNumber = $_.Extent.StartColumnNumber
+                    EndLineNumber     = $_.Extent.EndLineNumber
+                    EndColumnNumber   = $_.Extent.EndColumnNumber
+                    FileName          = Split-Path -Leaf $_.Extent.File
+                    FullName          = $_.Extent.File
+                }
+            }
+    }
+}
+
+function Find-VariableAssignment {
+    param(
+        $Name,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $Fullname
+    )
+
+    Process {
+        Select-Ast -FullName $FullName -ASTType AssignmentStatement |
+            Where {$_.VariableName -match $Name}
+    }
+}
+
+function ProcessAssignmentStatementAst ($item){
+    Get-ExtentInfo $item | 
+        Add-Member -PassThru -MemberType NoteProperty VariableName $item.Left
+}
+
+function Find-Variable {
+    param(
+        $Name,
+        [Parameter(ValueFromPipelineByPropertyName)]
+        $Fullname
+    )
+
+    Process {
+        Select-Ast -FullName $FullName -ASTType VariableExpression | ? {$_.VariableName -match $Name}
+    }
+}
+
+function ProcessVariableExpressionAst ($item){
+    Get-ExtentInfo $item |
+        Add-Member -PassThru -MemberType NoteProperty VariableName $item.VariablePath
+}
+
 cls
 
-dir *.ps1 | Select-Ast -ASTType FunctionDefinition # Command
+#dir . -r  *.ps1 | Find-Function select | Find-Usage
+#dir . -r  *.ps1 | Find-Usage Add-MenuItem | ft -a
+#dir . -r  *.ps1 | Find-FunctionUsage select-ast| ft -a
+
+dir . -r  *.ps1 | Find-Variable name| select v*name, *number | ft -a
+''
+dir . -r  *.ps1 | Find-VariableAssignment name| select v*name, *number, line, filename | ft -a
